676377734bf117cde42e64ce135ebacc318bcbc9
Mira-M <mira.mollar@gmail.com>
2015-08-08 12:48:36 -0400

    Updated minimum() function
    Works for Integer Values
    Works for Floating Point Values

--- MinValueStack/minvaluestack.py
+++ MinValueStack/minvaluestack.py
@@ -12,7 +12,7 @@
 
 class MinValue():
 
-    def __init__(self, item):
+    def __init__(self):
         self.myList = []
         self.mins = []
 
@@ -21,21 +21,52 @@ class MinValue():
             self.mins.append(item)
 
     def push(self, add, list):
+        return "testing"
 
 
     def minimum(self, item):
         self.myList = item
-        index = 0
-        
-        for item in self.myList:
-            if item > index:
-                myList
-    def tests(self):
-        print "+++++++++++ Test 1: Example Case from Brainstorming ++++++++++"
+        print "self.myList = " + str(self.myList)
+        min = self.myList[0]
 
+        for item in self.myList:
+            print "item = " + str(item)
+            if item < min:
+                min = item
+                print "min = " + str(min)
+
+        self.mins.append(min)
+        return min
+
+
+    def tests(self):
+        print "+++++++++++ Test 1: Example Cases from Brainstorming ++++++++++"
+
+        print "thisList = [1 ,2 ,3,4]"
         thisList = [1 ,2 ,3,4]
 
-        print "push(4, thisList)"
+        print "minimum(thisList)"
+        minimumValue = self.minimum(thisList)
+
+        print "Minimum Value = " + str(minimumValue)
+
+        print "thisList = [4 ,3 ,2 ,1]"
+        thisList = [4 ,3 ,2 ,1]
+
+        print "minimum(thisList)"
+        minimumValue = self.minimum(thisList)
+
+        print "Minimum Value = " + str(minimumValue)
+
+        print "+++++++++++++++++ Test 2: Float Data Type +++++++++++++++++++++"
+
+        print "thisList = [3.4 ,2.7 ,7.7 ,1.2]"
+        thisList = [3.4 ,2.7 ,7.7 ,1.2]
+
+        print "minimum(thisList)"
+        minimumValue = self.minimum(thisList)
+
+        print "Minimum Value = " + str(minimumValue)
 
 
 if __name__ == '__main__':
